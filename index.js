77e425d697c6bb907cdab785a9821bf6777103e4
recordboy <ksj_design@naver.com>
2020-11-05 14:09:04 +0900

    concurrently 모듈 추가

--- index.js
+++ index.js
@@ -12,3 +12,49 @@ app.listen(port);
 app.use('/api/data', function(req, res) {
     res.json({ greeting: 'Hello World' });
 });
+
+console.log(`server running at http ${port}`);
+
+
+
+
+
+// const express = require('express');
+// const path = require('path');
+// const fetch = require("node-fetch");
+// const app = express();
+
+// // 크로스 설정
+// const cors = require("cors");
+// app.use(cors());
+
+// // Serve static files from the React app
+// app.use(express.static(path.join(__dirname, 'client/build')));
+
+// // Put all API endpoints under '/api'
+// app.get('/api/data', (req, res) => {
+
+//   // 장소 id
+//   const id = req.param('id');
+
+//   fetch(`https://place.map.kakao.com/m/main/v/${id}`)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (myJson) {
+//       const data = JSON.parse(JSON.stringify(myJson));
+//       res.json(data);
+//     });
+  
+// });
+
+// // The "catchall" handler: for any request that doesn't
+// // match one above, send back React's index.html file.
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname+'/client/build/index.html'));
+// });
+
+// const port = process.env.PORT || 5000;
+// app.listen(port);
+
+// console.log(`server running at http ${port}`);
